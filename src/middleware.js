1684b40f085481c12bf7126d3f5fee3c4f57cdc6
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-12-07 17:54:09 +0000

    Added middleware for auth

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+// src/middleware.js
+import { NextResponse } from "next/server";
+
+// Read username and password from environment variables
+const USERNAME = process.env.BASIC_AUTH_USERNAME;
+const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
+export function middleware(req) {
+  const authHeader = req.headers.get("authorization");
+
+  if (authHeader) {
+    const [scheme, encoded] = authHeader.split(" ");
+    if (scheme === "Basic") {
+      const decoded = Buffer.from(encoded, "base64").toString(); // "username:password"
+      const [user, pass] = decoded.split(":");
+
+      if (user === USERNAME && pass === PASSWORD) {
+        return NextResponse.next(); // Auth success
+      }
+    }
+  }
+
+  // If auth fails or missing
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+  });
+}
